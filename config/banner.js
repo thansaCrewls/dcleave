23bf5e089f05a128e6a42de3b2af9109d2291ea8
Sulthan <64415680+thansaCrewls@users.noreply.github.com>
2024-11-26 14:59:58 +0700

    Add files via upload

---
+++ config/banner.js
@@ -0,0 +1,10 @@
+const banner = `
+\x1b[34m╔══════════════════════════════════════════════╗
+║             DISCORD SERVER LEAVER            ║
+╚══════════════════════════════════════════════╝\x1b[0m
+
+\x1b[90mDiscord Server Management Tool v696969
+Created by: Thansa\x1b[0m
+`;
+
+module.exports = banner;
